0a28a400035af62646cc91d5d5fb287a424b20a1
marloncristo <marlon.cristo.paula@gmail.com>
2021-10-15 20:22:56 -0300

    Validando contrato com Json Schema e isolando configs com Owner

--- src/test/java/com/montanha/isolada/ViagensTest.java
+++ src/test/java/com/montanha/isolada/ViagensTest.java
@@ -1,24 +1,19 @@
 package com.montanha.isolada;
 
+import com.montanha.config.Configuracoes;
 import com.montanha.factory.UsuarioDataFactory;
 import com.montanha.factory.ViagemDataFactory;
 import com.montanha.pojo.Usuario;
 import com.montanha.pojo.Viagem;
-import io.restassured.http.ContentType;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
-import static javax.swing.text.DefaultStyledDocument.ElementSpec.ContentType;
 import static org.hamcrest.Matchers.*;
-
-
-import static io.restassured.RestAssured.*;
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
 public class ViagensTest
 {
@@ -26,9 +21,11 @@ public class ViagensTest
     @Before
     public void setUP() {
         // configuracao rest-assured
-        baseURI = "http://localhost";
-        port = 8089;
-        basePath = "/api";
+
+        Configuracoes configuracoes = ConfigFactory.create(Configuracoes.class);
+        baseURI = configuracoes.baseURI();
+        port = configuracoes.port();
+        basePath = configuracoes.basePath();
 
         Usuario usuarioAdministrador = UsuarioDataFactory.criarUsuarioAdministrador();
 
@@ -57,7 +54,7 @@ public class ViagensTest
             .assertThat()
                 .statusCode(201)
                 .body("data.localDeDestino",equalTo("Itaperuçu"))
-                .body("data.acompanhante", equalToIgnoringCase("marlon"));
+                .body("data.acompanhante", equalToIgnoringCase("Jamile"));
     }
 
     @Test
@@ -75,5 +72,22 @@ public class ViagensTest
                 .statusCode(400)
                 .body("errors.defaultMessage",
                         hasItem("Local de Destino deve estar entre 3 e 40 caracteres"));
+    }
+
+    @Test
+    public void testCadastroViagemValidaContrato() throws IOException {
+
+        Viagem viagemValida = ViagemDataFactory.criarViagemValida();
+
+        given()
+            .contentType(io.restassured.http.ContentType.JSON)
+            .body(viagemValida)
+            .header("Authorization", token)
+        .when()
+            .post("/v1/viagens")
+        .then()
+            .assertThat()
+                .statusCode(201)
+                .body(matchesJsonSchemaInClasspath("schemas/postV1ViagensViagemValida.json"));
     }
 }
